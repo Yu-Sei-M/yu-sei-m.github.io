30c2c360c99b4cd607157f77736bf7946b6ab151
Eihara <qritarea@gmail.com>
2020-11-02 20:39:00 +0900

    deployment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://yu-sei-m.github.io/react"
           target="_blank"
           rel="noopener noreferrer"
         >
